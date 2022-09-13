94968a56d8bebd8da0d17dcf0f226d0f7103efe9
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-09-13 22:14:15 +0530

    datas adding

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import Navbar from  '../../Components/Navbar/Navbar'
+import '../List/list.scss'
 
 const List = () => {
   return (
-    <div>List</div>
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+       {/* <Datatable/> */}
+      </div>
+    </div>
   )
 }
 
